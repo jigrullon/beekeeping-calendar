e56f086821d0cd0c3c1581bd9a3851733ff99696
jgrullon <jaimeigrullon@gmail.com>
2025-09-10 07:03:12 -0400

    Fixing typing and routing so it shows images properly

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/beek' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/beek' : '',
+  //assetPrefix: process.env.NODE_ENV === 'production' ? '/beek' : '',
   trailingSlash: true,
   images: {
     unoptimized: true
